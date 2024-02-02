e74c934a9598c0c735eb0f4e1e485f59a123a86f
SandyFit <sandy07r@gmail.com>
2024-02-02 00:28:22 -0500

    Improve TeamBigCard mobile UI - Ensure responsive width and padding for better screen utilization - Adjust font sizes for mobile readability - Enhance modal presentation for a more intuitive mobile experience

--- src/components/cards/TeamBigCard.jsx
+++ src/components/cards/TeamBigCard.jsx
@@ -1,7 +1,7 @@
 import { Close } from '@mui/icons-material';
 import React from 'react'
 
-const TeamBigCard = ({ firstName, lastName, bio, isOpen, onClose, openMobileModal }) => {
+const TeamBigCard = ({ firstName, lastName, bio, isOpen, onClose }) => {
 
   const handleClose = () => {
     onClose();
@@ -12,18 +12,18 @@ const TeamBigCard = ({ firstName, lastName, bio, isOpen, onClose, openMobileModa
   return (
     <article 
       style={{ display: isOpen ? "block" : "none" }} // Direct inline style based on `isOpen`
-      className={`bg-spaceCadet w-[22rem] lg:w-[35rem] h-auto lg:h-[42rem] absolute lg:left-[58rem] 
-      lg:top-[6rem] rounded-xl xl:rounded-[38px] flex flex-col justify-center items-center 
+      className={`bg-spaceCadet w-[22rem] lg:w-[35rem] h-auto lg:h-[42rem] absolute left-[-2rem] lg:left-[58rem] 
+      top-[2rem] lg:top-[6rem] rounded-xl xl:rounded-[38px] flex flex-col justify-center items-center 
       px-14 py-8 space-y-8 shadow-xl shadow-midnight z-20 lg:z-1`}
     >
 
 
-      <button className="lg:hidden text-spanishBlue"
+      <button className="lg:hidden text-spanishBlue absolute left-[19.5rem] top-4"
               onClick={handleClose}
       >
         <Close/>
       </button>
-      <h2 className='text-center text-white2 text-xl md:text-2xl xl:text-4xl font-bold'>
+      <h2 className='text-center text-white2 text-2xl xl:text-4xl font-bold'>
         {firstName}
         <span className='text-azure' style={{ margin: ' 0 1rem' }}>
           {lastName}
