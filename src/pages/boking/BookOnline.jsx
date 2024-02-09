7c1ce7f6e4b0846fcc9297b75a67a54cbb66bc35
SandyFit <sandy07r@gmail.com>
2024-02-09 16:58:23 -0500

    Refined the weekly calendar styles and ensured today's date is highlighted

--- src/pages/boking/BookOnline.jsx
+++ src/pages/boking/BookOnline.jsx
@@ -1,50 +1,46 @@
-import React from 'react'
+import React from 'react';
 import BookingNavbar from '../../layouts/BookingNavbar';
 import BookingFooter from '../../layouts/BookingFooter';
 import { useNavigate } from 'react-router-dom';
+import BookingContactCard from '../../components/cards/BookingContactCard';
 
 const BookOnline = () => {
     const navigate = useNavigate();
 
-
     return (
-        <main className='flex flex-col min-h-screen'>
+        <main className='flex flex-col h-screen'>
             <BookingNavbar />
-            <section className='flex flex-grow justify-between'>
-                <div className='w-3/4 flex flex-col'>
-                    {/* Adjusted Content Section for Centering */}
-                    <div className="flex-1 flex flex-col justify-center items-center py-6">
-                        <h2 className='text-spaceCadet text-xl md:text-3xl xl:text-4xl font-bold text-left w-[55%]'>
+            <section className='flex flex-grow overflow-hidden'>
+                {/* Left Article: Scrollable Content */}
+                <article className='w-3/4 overflow-auto'>
+                    <div className="flex flex-col justify-center relative px-36 mt-12 pb-28">
+                        <hr className='border-t-2 border-slate-300 w-full my-6' />
+                        <div> {/* This is the parent flex container */}
+                            <BookingContactCard />
+                        </div>
+                        <hr className='border-t-2 border-slate-300 w-full my-6' />
+                        <h2 className='text-spaceCadet text-xl md:text-3xl xl:text-4xl font-bold text-left w-full mt-12'>
                             What Type of Appointment Would You Like to Schedule?
                         </h2>
                         <div className="space-y-4 mt-4">
-                            <button className="booking-btn"
-                                onClick={() => navigate('/new-patient-appointment')}
-                            >
+                            <button className="booking-btn" onClick={() => navigate('/new-patient-appointment')}>
                                 New Patient
                             </button>
-                            <button className="booking-btn"
-                                onClick={() => navigate('/existing-patient-appointment')}
-                            >
+                            <button className="booking-btn" onClick={() => navigate('/existing-patient-appointment')}>
                                 Existing Patient
                             </button>
-                            <button className="booking-btn"
-                                onClick={() => navigate('/emergency')}
-                            >
+                            <button className="booking-btn" onClick={() => navigate('/emergency')}>
                                 Emergency/Toothache
                             </button>
                         </div>
                     </div>
+                    <BookingFooter />
+                </article>
 
-                    {/* Footer Section */}
-                    <div className="">
-                        <BookingFooter/>
-                    </div>
-                </div>
-
-                <div className='w-auto'> {/* Adjusted width for the right div if you're using a 3/4 and 1/4 layout */}
-                    <img src="/map.jpg" alt="Radiant Google Map" className='w-full h-full object-cover' />
-                </div>
+                {/* Right Article: Fixed Content */}
+                <article className='overflow-hidden'>
+                    <img src="/map.jpg" alt="Radiant Google Map" className='w-full h-screen object-cover' />
+                </article>
             </section>
         </main>
     );
--- src/pages/boking/NewPatientAppointment.jsx
+++ src/pages/boking/NewPatientAppointment.jsx
@@ -30,12 +30,12 @@ const NewPatientAppoinment = () => {
     }
 
     return (
-        <main className='flex flex-col min-w-screen h-screen overflow-hidden'>
+        <main className='flex flex-col min-w-screen h-screen '>
             <BookingNavbar />
-            <section className='flex flex-grow justify-between overflow-hidden'>
-                <article className='w-3/4 flex flex-col overflow-y-auto max-h-screen '>
+            <section className='flex flex-grow justify-between '>
+                <article className='w-full flex flex-col  max-h-screen '>
                     {/* Adjusted Content Section for Centering */}
-                    <div className="flex-1 flex flex-col justify-center relative px-36 py-28">
+                    <div className="flex-1 flex flex-col justify-center relative px-48 py-28">
                         <button className="absolute top-8 text-lg font-medium text-blue-400"
                             onClick={() => navigate(-1)} 
                         >
@@ -45,6 +45,22 @@ const NewPatientAppoinment = () => {
                             <h2 className='text-spaceCadet text-xl md:text-3xl xl:text-4xl font-bold text-left w-[65%]'>
                                 Select an Appointment
                             </h2>
+
+                            
+                        </div>
+                      
+                        <hr className='border-t-2 border-slate-300 w-[100%] my-6' />
+
+                        <div className="flex justify-between w-full mb-4">
+                                                 
+                            <button
+                                onClick={handleCalendar}
+                                className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md 
+                                bg-spanishBlue px-12 text-xl uppercase font-semibold text-neutral-50">
+                                <span className="absolute h-0 w-0 bg-blue-500 
+                                    transition-all duration-300 group-hover:h-full group-hover:w-full"></span>
+                                <span className="relative">Select Date <CalendarMonth/></span>
+                            </button>
 
                             <select id="appointment"
                                 className="input w-80"
@@ -56,28 +72,9 @@ const NewPatientAppoinment = () => {
                                 <option value="existing">Existing Patient</option>
                                 <option value="emergency">Emergency</option>
                             </select>
-                        </div>
-                      
-                        <hr className='border-t-2 border-slate-300 w-[100%] my-6' />
-                        
-                        <div > {/* This is the parent flex container */}
-                            <BookingContactCard/>
-                        </div>
-
-                        <hr className='border-t-2 border-slate-300 w-[100%] my-6' />
-
-                        <div className="flex mt-6 mb-10">                            
-                            <button
-                                onClick={handleCalendar}
-                                className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md 
-                                bg-spanishBlue px-12 text-xl uppercase font-semibold text-neutral-50">
-                                <span className="absolute h-0 w-0 bg-blue-500 
-                                    transition-all duration-300 group-hover:h-full group-hover:w-full"></span>
-                                <span className="relative">Select Date <CalendarMonth/></span>
-                            </button>
                         </div>                                                   
                             
-                        <div className="absolute bottom-[28.5rem] z-50">
+                        <div className="absolute bottom-[42rem] z-50">
                             {openCalendar && (
                                         <Calendar
                                     onChange={handleAppointmentChange} // Corrected props for handling date change
@@ -87,8 +84,9 @@ const NewPatientAppoinment = () => {
                         
                         </div>
                         
-
-                        <BookingCalendar/>
+                        <div className="flex justify-end my-12">
+                            <BookingCalendar/>                           
+                        </div>
                         
                         {/* <hr className='border-t-2 border-slate-300 w-[100%] my-6' /> */}
                         
@@ -111,9 +109,9 @@ const NewPatientAppoinment = () => {
                     </div>
                 </article>
 
-                <article className='overflow-hidden'> {/* Adjusted width for the right article if you're using a 3/4 and 1/4 layout */}
+                {/* <article className='overflow-hidden'> 
                     <img src="/map.jpg" alt="Radiant Google Map" className='w-full h-screen object-cover' />
-                </article>
+                </article> */}
             </section>
         </main>
     );
